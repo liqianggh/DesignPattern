534a2aeec3bd085d7e50c056d4673df26ebe9d24
liqianggh <liqiang1chn@163.com>
2019-08-16 00:02:10 +0800

    Enhancement:1. 单例模式

--- src/cn/mycookies/test02singleton/type04/Singleton.java
+++ src/cn/mycookies/test02singleton/type04/Singleton.java
@@ -8,29 +8,24 @@ package cn.mycookies.test02singleton.type04;
  **/
 public enum Singleton {
 
-    INSTANCE;
+    INSTANCE("Jann", 23);
 
     private String name;
 
     private int age;
 
-    Singleton() {
+    Singleton(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
 
